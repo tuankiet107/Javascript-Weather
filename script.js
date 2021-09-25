465c63807a40eb7a7584924bbd44935c6e896e19
Tuan Kiet <thongtuankiet.1999@gmail.com>
2021-09-25 17:27:19 +0700

    add icons

--- script.js
+++ script.js
@@ -1,25 +1,26 @@
-const wrapper = document.querySelector('.wrapper');
-const searchInput = wrapper.querySelector('.search-input');
-const inputField = searchInput.querySelector('input');
-const searchButton = searchInput.querySelector('button');
+const wrapper = document.querySelector(".wrapper");
+const searchInput = wrapper.querySelector(".search-input");
+const inputField = searchInput.querySelector("input");
+const searchButton = searchInput.querySelector("button");
 
-const infoTxt = wrapper.querySelector('.info-txt');
+const infoTxt = wrapper.querySelector(".info-txt");
+const imgIcon = document.querySelector(".img-icon img");
 
-const apiKey = 'YOUR API KEY HERE';
+const apiKey = "YOUR API KEY HERE";
 let api;
 
-inputField.addEventListener('keyup', (e) => {
-  if (e.key == 'Enter' && inputField.value !== '') {
+inputField.addEventListener("keyup", (e) => {
+  if (e.key == "Enter" && inputField.value !== "") {
     requestApi(inputField.value);
   }
 });
 
-searchButton.addEventListener('click', () => {
-  if (inputField.value === '') {
+searchButton.addEventListener("click", () => {
+  if (inputField.value === "") {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(onSuccess, onError);
     } else {
-      alert('Your browser not support geolocation api');
+      alert("Your browser not support geolocation api");
     }
   } else {
     requestApi(inputField.value);
@@ -29,14 +30,14 @@ searchButton.addEventListener('click', () => {
 function onSuccess(position) {
   const { latitude, longitude } = position.coords;
   api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&&appid=${apiKey}`;
-  console.log('success');
+  console.log("success");
   fetchData();
 }
 
 function onError(err) {
   infoTxt.innerText = err.message;
-  infoTxt.classList.add('error');
-  console.log('error');
+  infoTxt.classList.add("error");
+  console.log("error");
 }
 
 async function requestApi(city) {
@@ -45,8 +46,8 @@ async function requestApi(city) {
 }
 
 async function fetchData() {
-  infoTxt.innerText = 'Getting weather details...';
-  infoTxt.classList.add('pending');
+  infoTxt.innerText = "Getting weather details...";
+  infoTxt.classList.add("pending");
 
   const res = await fetch(api);
   const data = await res.json();
@@ -56,20 +57,37 @@ async function fetchData() {
 }
 
 function weatherDetails(data) {
-  infoTxt.classList.replace('pending', 'error');
-  if (data.cod === '404') {
+  infoTxt.classList.replace("pending", "error");
+  if (data.cod === "404") {
     infoTxt.innerText = data.message;
   } else {
-    infoTxt.classList.remove('pending', 'error');
+    infoTxt.classList.remove("pending", "error");
     const { description, id } = data.weather[0];
     const { name } = data;
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
+
+    if (id === 800) {
+      imgIcon.src = "icons/clear.svg";
+    } else if (id >= 200 && id <= 232) {
+      imgIcon.src = "icons/clear.svg";
+    } else if ((id >= 300 && id <= 321) || (id >= 500 && id <= 531)) {
+      imgIcon.src = "icons/rain.svg";
+    } else if (id >= 600 && id <= 622) {
+      imgIcon.src = "icons/snow.svg";
+    } else if (id >= 701 && id <= 781) {
+      imgIcon.src = "icons/haze.svg";
+    } else if (id >= 801 && id <= 804) {
+      imgIcon.src = "icons/cloud.svg";
+    }
+
     console.log(data);
-    wrapper.querySelector('.weather .city').innerText = 'Weather in ' + name;
-    wrapper.querySelector('.weather .temp').innerText = Math.floor(temp) + '°C';
-    wrapper.querySelector('.weather .description').innerText = description;
-    wrapper.querySelector('.weather .humidity').innerText = 'Humidity: ' + humidity + '%';
-    wrapper.querySelector('.weather .wind').innerText = 'Wind speed: ' + speed + 'km/h';
+    wrapper.querySelector(".weather .city").innerText = "Weather in " + name;
+    wrapper.querySelector(".weather .temp").innerText = Math.floor(temp) + "°C";
+    wrapper.querySelector(".weather .description").innerText = description;
+    wrapper.querySelector(".weather .humidity").innerText =
+      "Humidity: " + humidity + "%";
+    wrapper.querySelector(".weather .wind").innerText =
+      "Wind speed: " + speed + "km/h";
   }
 }
